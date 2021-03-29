c45af07566f2a52b91d70eceb9209283e4174878
Prashant <37273899+OculusMode@users.noreply.github.com>
2021-03-29 21:32:23 +0530

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff not in hashMap:
+                hashMap[val] = i
+            else:
+                return [hashMap[diff], i]
+        return []
